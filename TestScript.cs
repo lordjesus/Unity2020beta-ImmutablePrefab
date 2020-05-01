f3994c3055270bd8bd1bfb2344f8f2650f84ef79
Daniel Jallov <danieljallov@gmail.com>
2020-05-01 14:45:45 +0200

    Initial commit

---
+++ TestScript.cs
@@ -0,0 +1,20 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TestScript : MonoBehaviour
+{
+    public int Value;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+	print("Value: " + Value);        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
